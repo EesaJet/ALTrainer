16b614801d30d167358273729210d4e4eb98a124
EesaJet <89559265+EesaJet@users.noreply.github.com>
2022-01-07 16:11:50 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  prefix: "!",
+  token: "OTI4ODA5OTk1MDc0Njk1MjA5.YdeLtw.QvFniSAzVgo_XWJ22bjF7iu3OFU",
+  botOwner: "710860868850716666",
+  channelMax: 2,
+  clientId: "713331743214825522",
+  supportGuildId: "719541290580284557",
+  topggAPIkey: "top.gg API key",
+  discordBotsggAPIkey: "discord.bots.gg API key",
+  discordBotscoAPIkey: "discordbots.co API key",
+};
